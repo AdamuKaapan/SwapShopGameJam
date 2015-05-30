92445a0539a91db0c0b7eb964a7eb248d0876eba
Adam Krpan <creeperinatardis@gmail.com>
2015-05-30 12:11:18 -0700

    Added a method to get individual sections of spritesheet.

--- src/com/swap/SpriteSheetUtil.java
+++ src/com/swap/SpriteSheetUtil.java
@@ -1,10 +1,18 @@
 package com.swap;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+
+import javax.imageio.ImageIO;
+
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.BufferedImageUtil;
 
 public class SpriteSheetUtil {
 	public static final String downloadPath = "CurrentSpritesheet.png";
@@ -22,5 +30,27 @@ public class SpriteSheetUtil {
 		} catch (IOException e) {
 			return false;
 		}
+	}
+	
+	public static Texture splitSpritesheet(int tile)
+	{
+		int tileX = tile % 8;
+		int tileY = tile / 8;
+		
+		try {
+			BufferedImage img = ImageIO.read(new File("res/" + downloadPath));
+			BufferedImage subimg = img.getSubimage(tileX * 16, tileY * 16, 16, 16);
+			return BufferedImageUtil.getTexture("SpriteSheetPart" + tile, subimg);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	public static Texture splitSpritesheet(int tileX, int tileY)
+	{
+		return splitSpritesheet((tileY * 8) + tileX);
 	}
 }
