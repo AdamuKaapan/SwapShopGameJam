1932a7a815caebcc871472d1c93a8c5e4dfb5256
Adam Krpan <creeperinatardis@gmail.com>
2015-05-30 10:21:41 -0700

    Initial Test Commit

---
+++ src/com/swap/Main.java
@@ -0,0 +1,28 @@
+package com.swap;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.create();
+			
+			while (!Display.isCloseRequested())
+			{
+				Display.update();
+				Display.sync(60);
+			}
+			
+			Display.destroy();
+			System.exit(0);
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
