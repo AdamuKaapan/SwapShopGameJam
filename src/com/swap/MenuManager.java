d735a99034e7e4e2a64facf72283714897a1e713
Adam Krpan <creeperinatardis@gmail.com>
2015-05-31 14:46:40 -0700

    Added range difficulty and value tweaks.

--- src/com/swap/MenuManager.java
+++ src/com/swap/MenuManager.java
@@ -399,7 +399,7 @@ public class MenuManager {
 	public static void newSpriteSheet(){
 		while(!SpriteSheetUtil.downloadSpritesheet() || SpriteSheetUtil.getViableLevels() == 0){}
 		currentDifficulty = 0;
-		for(int i = 0; i < 64; i++) currentDifficulty += SpriteSheetUtil.getSpriteSheetPart(i).getDifficulty();
+		for(int i = 0; i < 64; i++) currentDifficulty += SpriteSheetUtil.getSpriteSheetPart(i).getClumpDifficulty();
 		currentDifficulty /= 64;
 	}
 	
