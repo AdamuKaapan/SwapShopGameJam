d32e38fdf1d2efdbe7069823223262396ed26bd8
osreboot <calvin.16.weaver@gmail.com>
2015-05-30 19:08:38 -0700

    Added SoundManager yas

---
+++ src/com/swap/SoundManager.java
@@ -0,0 +1,28 @@
+package com.swap;
+
+import org.newdawn.slick.openal.Audio;
+
+import com.osreboot.ridhvl.loader.HvlSoundLoader;
+
+public class SoundManager {
+
+	private static HvlSoundLoader songSoundLoader = new HvlSoundLoader(5);
+	private static HvlSoundLoader effectSoundLoader = new HvlSoundLoader(20);
+	
+	public enum SoundSeries{
+		SONG, EFFECT
+	}
+	
+	public static void initialize(){
+		songSoundLoader.loadResource("Swapsong");
+	}
+	
+	public static Audio getSound(SoundSeries seriesArg, int indexArg){
+		switch(seriesArg){
+		case SONG: return songSoundLoader.getResource(indexArg);
+		case EFFECT: return effectSoundLoader.getResource(indexArg);
+		default: return null;
+		}
+	}
+	
+}
